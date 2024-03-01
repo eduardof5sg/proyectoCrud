08f30fad4070c9c7e340363d2f5260e36991a5e1
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2024-03-01 10:03:02 +0100

    Update db.js
    as

--- node/database/db.js
+++ node/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('database_app','root','aitOreduardOwhilak',{
+const db = new Sequelize('database_app','root','eduardo20',{
     host:'localhost',
     dialect:'mysql'
 })
